042e5770f1beb18b969e5c90675b4cf39c44be5d
GaneshHiradeve <ganeshhiradeve2@gmail.com>
2023-06-17 13:50:51 +0530

    working on it

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <Router>
         <Header />
         <Routes>
