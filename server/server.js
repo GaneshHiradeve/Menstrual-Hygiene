70a71e834641cece7de7b6464d4408dc741d5a5f
Ganesh  Hiradeve <123239801+GaneshHiradeve@users.noreply.github.com>
2023-08-18 02:06:32 +0530

    delete

--- server/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { ConnectDB } from "./config/connectDB.js";
-import logger from "./utils/logger.js";
-
-ConnectDB();
-
-app.listen(process.env.PORT,()=>{
-    logger.info(`server is running on port ${process.env.PORT}`)
-})
